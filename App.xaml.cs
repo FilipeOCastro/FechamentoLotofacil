c76db7a771a0f0000058d55e8caa97add0f5905f
FilipeOCastro <filipe.oliveira.castro@gmail.com>
2026-01-30 18:57:06 -0300

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FechamentoLotofacil
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FechamentoLotofacil
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
